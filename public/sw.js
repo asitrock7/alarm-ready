ab018c00de1d5cc3443fc9ed1d8c48970231d6c0
asitrock7 <skyfiminternet@gmail.com>
2026-05-11 03:57:26 +0530

    Create sw.js

---
+++ public/sw.js
@@ -0,0 +1,9 @@
+self.addEventListener('push', (event) => {
+    const data = event.data.json();
+    self.registration.showNotification(data.title, {
+        body: data.body,
+        icon: '/icon.png',
+        vibrate: [200, 100, 200],
+        requireInteraction: true 
+    });
+});
